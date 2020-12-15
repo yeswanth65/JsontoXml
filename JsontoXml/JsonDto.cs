63072cf71f143feb45220cc86dcf4fbdd8341656
VenkataSaiYeswanth Devisetty <VenkataSaiYeswanth.Devisetty@aggne.com>
2020-12-15 18:03:27 +0530

    Add project files.

---
+++ JsontoXml/JsonDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsontoXml
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string DefinedType { get; set; }
+        public string Type { get; set; }
+        public string Required { get; set; }
+        public string Annotation { get; set; }
+    }
+
+
+    public class SubTag
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string DefinedType { get; set; }
+        public string Type { get; set; }
+        public string Required { get; set; }
+        public string Annotation { get; set; }
+        public List<Attribute> Attributes { get; set; }
+        public List<SubTag> SubTags { get; set; }
+    }
+
+    public class Root
+    {
+        public string name { get; set; }
+        public string status { get; set; }
+        public List<Attribute> attributes { get; set; }
+        public List<SubTag> subTags { get; set; }
+    }
+
+}
---
+++ JsontoXml/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace JsontoXml
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Devis\Downloads\Jsontobeconvertedtoxml.json";
+            
+            string json = File.ReadAllText(path);
+
+            var xmlobj = JsonConvert.DeserializeObject<Root>(json);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            doc.se
+
+            XDocument document = new XDocument();
+            List<XAttribute> attribList = new List<XAttribute>();
+            foreach ( var attrib in xmlobj.attributes)
+            {
+                attribList.Add(new XAttribute(attrib.Name, ""));
+            }
+            document.Add(new XElement(xmlobj.name, attribList));
+
+            var subtag = xmlobj.subTags;
+            var tempParentNode = xmlobj.name; 
+
+            while(subtag.Any())
+            {
+                foreach(var childsubtag in subtag)
+                {
+                    document.Element(tempParentNode).Add(new XElement(childsubtag.Name));
+                    if (childsubtag.SubTags.Any())
+                        tempParentNode = childsubtag.Name;
+                }
+                
+               
+            }
+
+
+
+            document.Save(Console.Out);
+
+             void AddSubtag(string parent, string child)
+            {
+                
+            }
+
+        }
+ 
+     
+
+
+
+    }
+}
